36d5c687f2269504ab26cb80b432f750e5654adc
ayumi_arai <0araiayumi@gmail.com>
2019-04-10 11:31:12 +0900

    リンクとログイン条件分岐追加

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,8 @@ module PictureFb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
 
-config.time_zone = 'Tokyo'
-config.active_record.default_timezone = :local
