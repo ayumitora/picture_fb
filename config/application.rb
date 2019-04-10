b2e36f4be8aee1e47622563ab89c2b5c4ccfcc09
ayumi_arai <0araiayumi@gmail.com>
2019-04-10 15:11:49 +0900

    アセットコンパイル

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module PictureFb
     config.load_defaults 5.1
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.assets.initialize_on_precompile = false
   end
 end
 
