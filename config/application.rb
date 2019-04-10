ed55643b4e574cd92e5f391334d2ff305685a8a5
ayumi_arai <0araiayumi@gmail.com>
2019-04-10 11:11:10 +0900

    例外処理追加

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,9 @@ module PictureFb
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+
   end
 end
+
+# config.time_zone = 'Tokyo'
+# config.active_record.default_timezone = :local
