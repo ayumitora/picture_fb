59d6dfb1a333184ed3b0cea07dfa2164937a25aa
ayumi_arai <0araiayumi@gmail.com>
2019-04-10 11:11:56 +0900

    タイムゾーン設定

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,5 @@ module PictureFb
   end
 end
 
-# config.time_zone = 'Tokyo'
-# config.active_record.default_timezone = :local
+config.time_zone = 'Tokyo'
+config.active_record.default_timezone = :local
